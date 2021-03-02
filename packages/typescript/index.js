a2cd8d51b2ad5fce14d407d64fc46f908fa62bdc
Soren Martius <soeren.martius@gmail.com>
2021-03-02 10:33:06 +0100

    feat: init basic, typescript and vue rules

---
+++ packages/typescript/index.js
@@ -0,0 +1,46 @@
+module.exports = {
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "@mineirosio/eslint-config-basic",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  rules: {
+    // TS
+    "no-useless-constructor": "off",
+    "@typescript-eslint/semi": ["error", "never"],
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/member-delimiter-style": [
+      "error",
+      { multiline: { delimiter: "none" } },
+    ],
+    "@typescript-eslint/type-annotation-spacing": ["error", {}],
+
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      2,
+      { args: "none", ignoreRestSiblings: true },
+    ],
+    "no-redeclare": "off",
+    "@typescript-eslint/no-redeclare": "error",
+
+    // off
+    "@typescript-eslint/camelcase": "off",
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-parameter-properties": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/ban-ts-ignore": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "@typescript-eslint/ban-types": "off",
+  },
+};

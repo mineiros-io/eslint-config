7e1e11bb75960a2cc41c48c74af3a210fc3db5fc
Soren Martius <soeren.martius@gmail.com>
2021-07-04 18:18:12 +0200

    refactor: refactor rules, plugins and inerhit overwrites

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -1,6 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const basic = require("@mineiros/eslint-config-basic")
+
 module.exports = {
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
   extends: [
     "@mineiros/eslint-config-basic",
     "plugin:@typescript-eslint/recommended",
@@ -10,28 +11,24 @@ module.exports = {
       typescript: {},
     },
   },
+  overrides: basic.overrides,
   rules: {
     // TS
     "no-useless-constructor": "off",
     "@typescript-eslint/semi": ["error", "never"],
-    "@typescript-eslint/indent": ["error", 2],
-    "@typescript-eslint/member-delimiter-style": [
-      "error",
-      { multiline: { delimiter: "none" } },
-    ],
+    "@typescript-eslint/member-delimiter-style": ["error", { multiline: { delimiter: "none" } }],
     "@typescript-eslint/type-annotation-spacing": ["error", {}],
 
+    "indent": "off",
+    "@typescript-eslint/indent": ["error", 2],
     "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      2,
-      { args: "none", ignoreRestSiblings: true },
-    ],
+    "@typescript-eslint/no-unused-vars": "error",
     "no-redeclare": "off",
     "@typescript-eslint/no-redeclare": "error",
 
-    // off
+    // Deactivate Rules
     "@typescript-eslint/camelcase": "off",
-    // '@typescript-eslint/explicit-function-return-type': 'off',
+    "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-member-accessibility": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-parameter-properties": "off",
@@ -40,7 +37,7 @@ module.exports = {
     "@typescript-eslint/no-empty-function": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/ban-ts-comment": "off",
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/ban-types": "off",
   },
 };

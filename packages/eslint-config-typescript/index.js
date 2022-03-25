b6bfd922c4abdd54ae715b8f43518b7cef0349e2
Soren Martius <soeren.martius@gmail.com>
2022-03-25 18:47:22 +0100

    feat: add more typescript rules

--- packages/eslint-config-typescript/index.js
+++ packages/eslint-config-typescript/index.js
@@ -13,8 +13,13 @@ module.exports = {
     // TS
     'no-useless-constructor': 'off',
     '@typescript-eslint/semi': ['error', 'never'],
+    '@typescript-eslint/ban-ts-comment': ['error', { 'ts-ignore': 'allow-with-description' }],
     '@typescript-eslint/member-delimiter-style': ['error', { multiline: { delimiter: 'none' } }],
     '@typescript-eslint/type-annotation-spacing': ['error', {}],
+    '@typescript-eslint/consistent-type-imports': ['error', { prefer: 'type-imports', disallowTypeAnnotations: false }],
+    '@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
+    '@typescript-eslint/consistent-indexed-object-style': ['error', 'record'],
+    '@typescript-eslint/prefer-ts-expect-error': 'error',
 
     'indent': 'off',
     '@typescript-eslint/indent': ['error', 2],
@@ -22,6 +27,14 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error', { functions: false, classes: false, variables: true }],
+    'brace-style': 'off',
+    '@typescript-eslint/brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'comma-dangle': 'off',
+    '@typescript-eslint/comma-dangle': ['error', 'always-multiline'],
+    'object-curly-spacing': 'off',
+    '@typescript-eslint/object-curly-spacing': ['error', 'always'],
 
     // Deactivate Rules
     '@typescript-eslint/camelcase': 'off',
@@ -33,7 +46,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-ignore': 'off',
     '@typescript-eslint/no-empty-function': 'off',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/ban-types': 'off',
     '@typescript-eslint/no-namespace': 'off',
